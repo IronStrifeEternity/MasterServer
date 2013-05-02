0dfed2c25b4df1391c764f33a46d162f0210f7bd
Eric Mellino <raverenx@gmail.com>
2013-05-01 21:37:36 -0400

    updated version

--- WpfApplication1/Matchmaking/Matchmaker.cs
+++ WpfApplication1/Matchmaking/Matchmaker.cs
@@ -144,6 +144,42 @@ namespace IronStrife.Matchmaking
             }
         }
 
+        public static Matchup FindMatchupFromIndex(List<MatchmakingEntity> usersInQueue, int index, int teamSize, int skillThreshold)
+        {
+            if (index >= usersInQueue.Count) { return null; }
+            var baseIndex = index;
+            var matchup = new Matchup(teamSize);
+            bool finished = false;
+            while (!finished)
+            {
+                if (index >= usersInQueue.Count)
+                {
+                    matchup = null; break;
+                }
+                var entity = usersInQueue[index];
+                matchup.TryAdd(entity);
+                if (matchup.TeamsFull)
+                {
+                    finished = true;
+                }
+                index++;
+            }
+            if (matchup == null)
+            {
+                return FindMatchupFromIndex(usersInQueue, baseIndex + 1, teamSize, skillThreshold);
+            }
+            else
+            {
+                Console.WriteLine("Matchup found: " + matchup.ToString());
+                matchup.OptimizeTeams();
+                if (matchup.IsMatchupBalanced(skillThreshold))
+                {
+                    return matchup;
+                }
+                else return FindMatchupFromIndex(usersInQueue, baseIndex + 1, teamSize, skillThreshold);
+            }
+        }
+
         private void ConnectUsersToGame(MatchmakingEntity entity, ServerInfo server, int teamNumber)
         {
             entity.SendMessage(ChatMessage.MatchFoundMessage(server, teamNumber));
